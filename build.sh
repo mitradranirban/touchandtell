5dec9ace3b38e87e02c316c233ea1e1a1c2f03b5
Dr Anirban Mitra <mitradranirban@gmail.com>
2025-06-11 22:46:50 +0530

    completed up to P

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-fontmake -m sources/V-love.designspace -o variable
-rm sources/ttx/V-love-VF.ttx 
-ttx -d  sources/ttx/ variable_ttf/V-love-VF.ttf
-echo "merge CPAL COLR and V-love-VF.ttx manually"
-code sources/ttx/*.ttx
-ttx -d fonts/variable_ttf ttx/V-love-VF.ttx
-ttx -d fonts/variable_ttf sources/ttx/V-love-VF.ttx
-ttx -d fonts/webfonts --flavor woff2 sources/ttx/V-love-VF.ttx
+fontmake -m sources/TouchandTell.designspace -o variable
+rm sources/ttx/TouchandTell-VF.ttx 
+ttx -d  sources/ttx/ variable_ttf/TouchandTell-VF.ttf
+echo "merge CPAL COLR and *-VF.ttx manually"
+codium sources/ttx/*.ttx
+ttx -d fonts/variable_ttf sources/ttx/TouchandTell-VF.ttx
+ttx -d fonts/webfonts --flavor woff2 sources/ttx/TouchandTell-VF.ttx
