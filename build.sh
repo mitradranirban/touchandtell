59c6df24dfb5ddb1c185326c7fde115b0204e5d8
Dr Anirban Mitra <mitradranirban@gmail.com>
2025-06-15 16:34:39 +0530

    completed up to v

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 fontmake -m sources/TouchandTell.designspace -o variable
-rm sources/ttx/TouchandTell-VF.ttx 
-ttx -d  sources/ttx/ variable_ttf/TouchandTell-VF.ttf
-echo "merge CPAL COLR and *-VF.ttx manually"
-codium sources/ttx/*.ttx
-ttx -d fonts/variable_ttf sources/ttx/TouchandTell-VF.ttx
-ttx -d fonts/webfonts --flavor woff2 sources/ttx/TouchandTell-VF.ttx
+# rm sources/ttx/TouchandTell-VF.ttx 
+cd sources/ttx 
+cp ../../variable_ttf/TouchandTell-VF.ttf .
+ttx -m TouchandTell-VF.ttf TouchandTell-COLR.ttx
+fonttools ttLib.woff2 compress -o TouchandTell.woff2  TouchandTell-COLR.ttf
+cd ../..
+rm -rf fonts 
+mkdir -p fonts/webfonts
+mv sources/ttx/TouchandTell-COLR.ttf fonts
+mv sources/ttx/TouchandTell.woff2 fonts/webfonts
